7ce3565440da73c621406e9d70306d13f19fd2e6
orinugul <ogu590626@gamil.com>
2024-06-17 17:49:12 +0900

    [Update] 플레이어 무브스크립트/애니메이터/애니메이션

--- Assets/Script/PlayerMove.cs
+++ Assets/Script/PlayerMove.cs
@@ -1,5 +1,9 @@
+using Mirror;
+using System.Collections;
+using UnityEditor;
 using UnityEngine;
-using UnityEngine.UIElements;
+using UnityEngine.InputSystem;
+using static UnityEngine.InputSystem.LowLevel.InputStateHistory;
 
 public class PlayerMove : MonoBehaviour
 {
@@ -11,14 +15,22 @@ public class PlayerMove : MonoBehaviour
     private Vector3 velocity;
     private float xRotation = 0f;
     private bool isGravityInverted = false;
-
+    private Vector2 moveInput = Vector2.zero;
+    private Vector2 moveVector = Vector2.zero;
     public Transform cameraTransform;
+    public bool isFire = false;
+    public bool isReload = false;
+    public bool canFire = true;
+    private float moveSpeed = 2f;
+    public Animator animator;
+    public int shell;
+    public int maxShell;
 
     void Start()
     {
         controller = GetComponent<CharacterController>();
-        UnityEngine.Cursor.lockState = CursorLockMode.Locked;  // 커서 잠금
-                                                               //   UnityEngine.Cursor.visible = false;  // 커서 숨기기
+        Cursor.lockState = CursorLockMode.Locked;  // 커서 잠금
+        // Cursor.visible = false;  // 커서 숨기기
     }
 
     void Update()
@@ -30,29 +42,10 @@ public class PlayerMove : MonoBehaviour
         xRotation -= mouseY;
         xRotation = Mathf.Clamp(xRotation, -90f, 90f);
 
-        cameraTransform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);    
+        cameraTransform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
         transform.Rotate(Vector3.up * mouseX);
 
-        // 키보드 입력을 통한 캐릭터 이동
-        float moveX = Input.GetAxis("Horizontal");
-        float moveZ = Input.GetAxis("Vertical");
-
-        Vector3 move = transform.right * moveX + transform.forward * moveZ;
-        controller.Move(move * speed * Time.deltaTime);
-
-        // Space 바를 입력하면 중력 반전 및 Z축 180도 회전
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            isGravityInverted = !isGravityInverted;
-            if (isGravityInverted)
-            {
-                transform.rotation = Quaternion.Euler(transform.eulerAngles.x, transform.eulerAngles.y, 180f);
-            }
-            else
-            {
-                transform.rotation = Quaternion.Euler(transform.eulerAngles.x, transform.eulerAngles.y, 0f);
-            }
-        }
+        MoveOrder();
 
         // 중력 적용
         if (controller.isGrounded && velocity.y < 0)
@@ -63,5 +56,79 @@ public class PlayerMove : MonoBehaviour
         float currentGravity = isGravityInverted ? -gravity : gravity;
         velocity.y += currentGravity * Time.deltaTime;
         controller.Move(velocity * Time.deltaTime);
+    }
+
+    void OnMove(InputValue inputValue) // 이동(WASD)
+    {
+        moveInput = inputValue.Get<Vector2>();
+    }
+
+    void OnSprint(InputValue inputValue)
+    {
+        float value = inputValue.Get<float>();
+        moveSpeed = (value * 2f) + 2f;
+    }
+
+    void OnFire(InputValue inputValue)
+    {
+        float isClick = inputValue.Get<float>();
+
+        if (isClick == 1)   // 눌렀을 때
+        {
+            isFire = true;
+        }
+        else // 뗄 때
+        {
+            isFire = false;
+        }
+    }
+    void GunFire()
+    {
+        if (canFire && isFire && shell > 0 && !isReload)
+        {
+            canFire= false;
+            //EffectManager.Instance.FireEffectGenenate(firePos.position, firePos.rotation);
+            animator.SetTrigger("Fire");
+            shell--;
+        }
+    }
+    public void FireDelay()
+    {
+        canFire = true;
+    }
+
+    void OnReload(InputValue inputValue)
+    {
+        float isClick = inputValue.Get<float>();
+
+        // 줌하고 있는 중에는 재장전 불가능
+        if (!isReload)
+        {
+            isReload = true;
+            Reload();
+        }
+    }
+    void Reload()
+    {
+            animator.SetTrigger("Reload");
+            //reload.PlayOneShot(reload.clip);
+            StartCoroutine(ReloadEnd());
+    }
+    IEnumerator ReloadEnd()
+    {
+        yield return new WaitForSeconds(1.4f);
+        isReload = false;
+        shell = maxShell;
+    }
+
+    private void MoveOrder()
+    {
+        moveVector = Vector2.Lerp(moveVector, moveInput * moveSpeed, Time.deltaTime * 5);
+
+        Vector3 moveVector3 = transform.right * moveVector.x + transform.forward * moveVector.y;
+        controller.Move(moveVector3 * Time.deltaTime);
+
+        animator.SetFloat("XSpeed", moveVector.x);
+        animator.SetFloat("ZSpeed", moveVector.y);
     }
 }
