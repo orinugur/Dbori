0a163ae87257947f5b2f80e9c76d1b5899aa0768
김민석 <ogu590626@gmail.com>
2024-10-31 10:37:47 +0900

    [update] 적변수수정

--- Assets/Prefabs/Coocker/AniEvent_Cooker.cs
+++ Assets/Prefabs/Coocker/AniEvent_Cooker.cs
@@ -9,8 +9,7 @@ public class AniEvent_Cooker : MonoBehaviour
     // Start is called before the first frame update
     void EndATK()
     {
-        BTA_Attack 
-            = GetComponent<BTA_Attack>(); // BTA_Attack 컴포넌트 가져오기
+        BTA_Attack attackTask = GetComponent<BTA_Attack>(); // BTA_Attack 컴포넌트 가져오기
         if (attackTask != null)
         {
             attackTask.EndAtk(); // EndAtk 호출
